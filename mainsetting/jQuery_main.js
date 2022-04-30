1b1e33da4857468dd19be1d907c45093084cc75e
eruboru <89179378+emesan22@users.noreply.github.com>
2022-04-30 11:01:45 +0900

    変更
    ・jsの読み込みをheadに変更
    ・その他変更\

--- mainsetting/jQuery_main.js
+++ mainsetting/jQuery_main.js
@@ -1,3 +1,35 @@
+
+$(function() {
+	var ref = document.referrer;             // リファラ情報を得る
+var hereHost = window.location.hostname; // 現在ページのホスト(ドメイン)名を得る
+ 
+// ホスト名が含まれるか探す正規表現を作る(大文字・小文字を区別しない)
+var sStr = "^https?://" + hereHost;
+var rExp = new RegExp( sStr, "i" );
+ 
+// リファラ文字列を判別
+	if( ref.length == 0 ) {
+		setInterval(function() {
+			$('.first-page').fadeOut('fast');
+		},1000)
+		setInterval(function() {
+			$('.main-page').fadeIn('fast');
+		},1500)
+	}
+	else if( ref.match( rExp ) ) {
+		$('.first-page').css('display', 'none');
+		$('.main-page').show();
+	}
+	else {
+		setInterval(function() {
+			$('.first-page').fadeOut('fast');
+		},1000)
+		setInterval(function() {
+			$('.main-page').fadeIn('fast');
+		},1500)
+	}
+})
+
 $(function(){
 	var _window = $(window),
 		_header = $('header'),
